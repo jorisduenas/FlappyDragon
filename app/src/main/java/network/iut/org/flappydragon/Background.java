ca8bcca6837bf9df9ff8fa5034ee014706cca27f
Ancelin GLORIES <ancelin.glories@gmail.com>
2019-04-04 15:00:13 +0200

    Add : * new TestLayer
    Edit :
    * No actionbar

--- app/src/main/java/network/iut/org/flappydragon/Background.java
+++ app/src/main/java/network/iut/org/flappydragon/Background.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.util.Log;
 
 public class Background {
     private int height;
     private int width;
+    private Bitmap backgroundtest;
     private Bitmap background1;
     private Bitmap background2;
     private Bitmap background3;
@@ -17,18 +19,27 @@ public class Background {
     public Background(Context context) {
         height = context.getResources().getDisplayMetrics().heightPixels;
         width = context.getResources().getDisplayMetrics().widthPixels;
-        background1 = Util.decodeSampledBitmapFromResource(context.getResources(), R.drawable.layer1, width, height);
+        backgroundtest = Util.decodeSampledBitmapFromResource(context.getResources(), R.drawable.layertest, width, height);
+        Log.d("TEST", "h:"+height);
+        Log.d("TEST", "w:"+width);
+        /*background1 = Util.decodeSampledBitmapFromResource(context.getResources(), R.drawable.layer1, width, height);
         background2 = Util.decodeSampledBitmapFromResource(context.getResources(), R.drawable.layer2, width, height);
         background3 = Util.decodeSampledBitmapFromResource(context.getResources(), R.drawable.layer3, width, height);
         background4 = Util.decodeSampledBitmapFromResource(context.getResources(), R.drawable.layer4, width, height);
-        background5 = Util.decodeSampledBitmapFromResource(context.getResources(), R.drawable.layer5, width, height);
+        background5 = Util.decodeSampledBitmapFromResource(context.getResources(), R.drawable.layer5, width, height);*/
     }
 
     public void draw(Canvas canvas) {
-        canvas.drawBitmap(background1, new Rect(0, 0, background1.getWidth(), background1.getHeight()), new Rect(0, 0, width, height), null);
+        if(height > width) {
+            canvas.drawBitmap(backgroundtest, new Rect(0, 0, width, backgroundtest.getHeight()), new Rect(0, 0, width, height), null);
+        } else {
+            canvas.drawBitmap(backgroundtest, new Rect(0, 0, width + height, backgroundtest.getHeight()), new Rect(0, 0, width, height), null);
+        }
+
+        /*canvas.drawBitmap(background1, new Rect(0, 0, background1.getWidth(), background1.getHeight()), new Rect(0, 0, width, height), null);
         canvas.drawBitmap(background2, new Rect(0, 0, background1.getWidth(), background1.getHeight()), new Rect(0, 0, width, height), null);
         canvas.drawBitmap(background3, new Rect(0, 0, background1.getWidth(), background1.getHeight()), new Rect(0, 0, width, height), null);
         canvas.drawBitmap(background4, new Rect(0, 0, background1.getWidth(), background1.getHeight()), new Rect(0, 0, width, height), null);
-        canvas.drawBitmap(background5, new Rect(0, 0, background1.getWidth(), background1.getHeight()), new Rect(0, 0, width, height), null);
+        canvas.drawBitmap(background5, new Rect(0, 0, background1.getWidth(), background1.getHeight()), new Rect(0, 0, width, height), null);*/
     }
 }
--- app/src/main/java/network/iut/org/flappydragon/GameActivity.java
+++ app/src/main/java/network/iut/org/flappydragon/GameActivity.java
@@ -1,13 +1,20 @@
 package network.iut.org.flappydragon;
 
+import android.content.pm.ActivityInfo;
+import android.content.pm.ApplicationInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(new GameView(this));
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
 }
