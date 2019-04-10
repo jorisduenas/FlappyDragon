8e9db38196b36480099476b455d8af581cf84839
Joris <joris.duenas@gmail.com>
2019-04-10 17:50:52 +0200

    fix : macron

---
+++ app/src/main/java/network/iut/org/flappydragon/Macron.java
@@ -0,0 +1,64 @@
+package network.iut.org.flappydragon;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.util.Log;
+
+public class Macron {
+    public static Bitmap globalBitmap;
+    private final Bitmap bitmap;
+    private final byte frameTime;
+    private int frameTimeCounter;
+    private final int width;
+    private final int height;
+    private int x;
+    private int y;
+    private float speedX;
+    private float speedY;
+    private Context context;
+
+    public Macron(Context context, int y) {
+
+        this.context = context;
+
+        int height = context.getResources().getDisplayMetrics().heightPixels;
+        int width = context.getResources().getDisplayMetrics().widthPixels;
+
+        if(globalBitmap == null) {
+            Log.e("TEST", "Height : " + height + ", width : " + width);
+            globalBitmap = Util.decodeSampledBitmapFromResource(context.getResources(), R.drawable.pizza, Float.valueOf(height / 10f).intValue(), Float.valueOf(width / 10f).intValue());
+        }
+        this.bitmap = globalBitmap;
+        this.width = this.bitmap.getWidth();
+        this.height = this.bitmap.getHeight();
+        this.frameTime = 1;		// the frame will change every 3 runs
+        this.y = y;
+
+        this.x = this.width * 20;
+    }
+
+    public void move() {
+        changeToNextFrame();
+
+        this.speedX = -50;
+        this.speedY = 0;
+        this.x += speedX;
+        this.y += speedY;
+    }
+
+    protected void changeToNextFrame(){
+        this.frameTimeCounter++;
+        if(this.frameTimeCounter >= this.frameTime){
+            //TODO Change frame
+            this.frameTimeCounter = 0;
+        }
+    }
+
+    public int getY() {
+        return this.y;
+    }
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(bitmap, x, y , null);
+    }
+}
